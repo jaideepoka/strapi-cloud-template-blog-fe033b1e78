f80996f0f5d4a33bab048ed0a7017d63482973ee
Jaideep Oka <jaideepoka@outlook.com>
2025-07-29 15:52:31 +0530

    latest changes
    latest changes

---
+++ src/api/site-menu/controllers/site-menu.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * site-menu controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::site-menu.site-menu');
